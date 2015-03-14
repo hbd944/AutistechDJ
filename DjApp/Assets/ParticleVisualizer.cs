1fb3dcde178889da620024a219286e337419a409
Jeremy Carrillo <jecarrillo@csumb.edu>
2015-03-14 16:28:08 -0700

    no message

---
+++ DjApp/Assets/ParticleVisualizer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleVisualizer : MonoBehaviour {
+	ParticleSystem particles;
+	// Use this for initialization
+	void Start () {
+		particles = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(particles.emissionRate>=1000)
+			particles.emissionRate -= 1000;
+	
+	}
+	public void VisualizeOnBeat()
+	{
+		particles.emissionRate += 1000;
+	}
+}
