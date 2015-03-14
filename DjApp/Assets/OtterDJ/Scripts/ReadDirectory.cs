644d1042ff0ac97cd6a1469e3395f3524bd3d952
trippedoutfish <trippedoutfish@gmail.com>
2015-03-14 16:14:45 -0700

    Working more on directories

--- DjApp/Assets/OtterDJ/Scripts/ReadDirectory.cs
+++ DjApp/Assets/OtterDJ/Scripts/ReadDirectory.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine.UI;
+using System;
 
 public class ReadDirectory : MonoBehaviour {
 
@@ -11,13 +12,29 @@ public class ReadDirectory : MonoBehaviour {
 	public string CurrFile;
 	public string CurrDir;
 	public DirectoryInfo DirInfo;
+	public DirectoryInfo[] DirInfos;
+	public FileInfo[] FlInfo;
+	//public FileInfo file1;
+	public int currNum = 0;
 
 	public GameObject textObject;
 	// Use this for initialization
 	void Start () {
 		DirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
-		SongFiles = DirInfo.GetFiles();
-		foreach File in 
+		DirInfos = DirInfo.GetDirectories();
+		 
+		//SongFiles = DirInfo.GetFiles();
+		//for file finding within folder
+
+
+		FlInfo = DirInfo.GetFiles();
+		textObject.GetComponent<Text> ().text = Directory.GetCurrentDirectory().ToString();
+		if(FlInfo != null)
+		{
+			foreach(FileInfo file1 in FlInfo) {
+				SongFiles.Add(file1.ToString());
+			}
+		}
 	}
 	
 	// Update is called once per frame
@@ -27,7 +44,23 @@ public class ReadDirectory : MonoBehaviour {
 
 	void onClick() {
 		// CurrDir;
-
-		textObject.GetComponent<Text>().text = SongFiles[0].ToString();
+		/*if (currNum < DirInfos.Length) {
+			textObject.GetComponent<Text> ().text = DirInfos [currNum].Name;
+			currNum++;
+		} else {
+			currNum = 0;
+			textObject.GetComponent<Text> ().text = DirInfos [currNum].Name;
+			currNum++;
+		}*/
+			
+		 // Use this to iterate through all files in list
+		if (currNum < SongFiles.Count) {
+			textObject.GetComponent<Text> ().text = SongFiles [currNum];
+			currNum++;
+		} else {
+			currNum = 0;
+			textObject.GetComponent<Text> ().text = SongFiles [currNum];
+			currNum++;
+		}
 	}
 }
