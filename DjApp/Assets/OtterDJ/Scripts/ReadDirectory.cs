b0add674f59768b0bcc0a6b1a18b29678ad50492
trippedoutfish <trippedoutfish@gmail.com>
2015-03-14 13:55:06 -0700

    asd

--- DjApp/Assets/OtterDJ/Scripts/ReadDirectory.cs
+++ DjApp/Assets/OtterDJ/Scripts/ReadDirectory.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEngine.UI;
 
 public class ReadDirectory : MonoBehaviour {
 
@@ -9,14 +10,24 @@ public class ReadDirectory : MonoBehaviour {
 
 	public string CurrFile;
 	public string CurrDir;
+	public DirectoryInfo DirInfo;
 
+	public GameObject textObject;
 	// Use this for initialization
 	void Start () {
-		CurrDir = new DirectoryInfo(Directory.GetCurrentDirectory());
+		DirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+		SongFiles = DirInfo.GetFiles();
+		foreach File in 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void onClick() {
+		// CurrDir;
+
+		textObject.GetComponent<Text>().text = SongFiles[0].ToString();
 	}
 }
