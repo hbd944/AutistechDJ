6f45f0289ca1399cfca0444d184498a7d47e40b1
Alex Hauser <Alexhauser23@yahoo.com>
2015-03-14 13:00:33 -0700

    Added Audiosources and childed them to the main DJ Manager script

--- DjApp/Assets/DJManager.cs
+++ DjApp/Assets/DJManager.cs
@@ -6,9 +6,21 @@ public class DJManager : MonoBehaviour
 {
 	public List<AudioClip> songs = new List<AudioClip>();
 
+	public AudioClip clip;
+
+	public AudioSource audio1;
+	public AudioSource audio2;
+	public AudioSource audio3;
+	public AudioSource audio4;
+
+	public void PlaySong()
+	{
+
+	}
+
 	void Start () 
 	{
-	
+		Debug.Log (clip.name);
 	}
 	
 
