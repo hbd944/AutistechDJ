6d34a84563cd113356d3887ebd19d2d4e1e0e328
Alex Hauser <Alexhauser23@yahoo.com>
2015-03-14 11:55:55 -0700

    Stuff

---
+++ DjApp/Assets/DJManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DJManager : MonoBehaviour 
+{
+	public List<AudioClip> songs = new List<AudioClip>();
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+}
