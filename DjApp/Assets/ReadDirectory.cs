03cb4ecf96a356e4b00bddfcbd00bec427cc6d5b
trippedoutfish <trippedoutfish@gmail.com>
2015-03-14 13:30:53 -0700

    Added in scene and updated c#

--- DjApp/Assets/ReadDirectory.cs
+++ DjApp/Assets/ReadDirectory.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 public class ReadDirectory : MonoBehaviour {
 
+	public List<string> SongFiles = new List<string>();
+
+	public string CurrFile;
+	public string CurrDir;
+
 	// Use this for initialization
 	void Start () {
-	
+		CurrDir = new DirectoryInfo(Directory.GetCurrentDirectory());
 	}
 	
 	// Update is called once per frame
