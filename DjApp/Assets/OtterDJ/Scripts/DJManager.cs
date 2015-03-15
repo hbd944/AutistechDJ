37cee89ebfe9df16f8ca26cfe8176456c1b8c34b
Alex Hauser <Alexhauser23@yahoo.com>
2015-03-14 21:46:32 -0700

    BLa

--- DjApp/Assets/OtterDJ/Scripts/DJManager.cs
+++ DjApp/Assets/OtterDJ/Scripts/DJManager.cs
@@ -19,10 +19,121 @@ public class DJManager : MonoBehaviour
 
 	public AudioSource audio1;
 
+	public bool triggered;
+
+	public float cooldown;
+
+	public GameObject[] buttons;
+
 
 	public void PlaySong(AudioClip s)
 	{
 
+	}
+
+
+
+	public void LeftSwipe(float sw)
+	{
+		float farRight = 485;
+		foreach(GameObject go in buttons)
+		{
+			go.GetComponent<RectTransform>().Translate (new Vector3(sw*3,0,0));
+
+			if(go.GetComponent<RectTransform>().localPosition.x > 485)
+			{
+				if(go.GetComponent<RectTransform>().localPosition.x > farRight)
+				{
+					farRight = go.GetComponent<RectTransform>().localPosition.x;
+				}
+
+			}
+			if(go.GetComponent<RectTransform>().localPosition.x < -480)
+			{
+				if(farRight > 485)
+				{
+					go.GetComponent<RectTransform>().localPosition = new Vector3(farRight+200,go.GetComponent<RectTransform>().localPosition.y,go.GetComponent<RectTransform>().localPosition.z);
+			
+				
+				}
+				else
+				{
+					go.GetComponent<RectTransform>().localPosition = new Vector3(485,go.GetComponent<RectTransform>().localPosition.y,go.GetComponent<RectTransform>().localPosition.z);
+
+				}
+			}
+		}
+		//if(cooldown <= 0)
+		//{
+			//RotateLeft();
+
+		///	//cooldown = 2.0f;
+		//}
+	}
+
+	public void RightSwipe(float sw)
+	{
+		float farRight = 485;
+		foreach(GameObject go in buttons)
+		{
+			go.GetComponent<RectTransform>().Translate (new Vector3(sw,0,0));
+			
+			if(go.GetComponent<RectTransform>().localPosition.x > 485)
+			{
+				if(go.GetComponent<RectTransform>().localPosition.x > farRight)
+				{
+					farRight = go.GetComponent<RectTransform>().localPosition.x;
+				}
+				
+			}
+			if(go.GetComponent<RectTransform>().localPosition.x < -480)
+			{
+				if(farRight > 485)
+				{
+					go.GetComponent<RectTransform>().localPosition = new Vector3(farRight+150,go.GetComponent<RectTransform>().localPosition.y,go.GetComponent<RectTransform>().localPosition.z);
+					
+					
+				}
+				else
+				{
+					go.GetComponent<RectTransform>().localPosition = new Vector3(485,go.GetComponent<RectTransform>().localPosition.y,go.GetComponent<RectTransform>().localPosition.z);
+					
+				}
+			}
+		}
+		//if(cooldown <= 0)
+		//{
+		//RotateLeft();
+		
+		///	//cooldown = 2.0f;
+		//}
+	}
+
+	public void RotateLeft()		
+	{
+	//	slot0.GetComponent<ButtonAnimator>().MoveSixToOne ();
+	//	slot1.GetComponent<ButtonAnimator>().MoveTwoToThree ();
+	//	slot2.GetComponent<ButtonAnimator>().MoveTwoToThree();
+	//	slot3.GetComponent<ButtonAnimator>().MoveTwoToThree();
+	//	slot1.GetComponent<RectTransform>().position = Vector3.zero;
+
+		slot0.SendMessage ("MoveOneToTwo");
+		slot1.SendMessage ("MoveTwoToThree");
+		slot2.SendMessage ("MoveThreeToFour");
+		slot3.SendMessage ("MoveFourToFive");
+		slot4.SendMessage ("MoveFiveToSix");
+		slot5.SendMessage ("MoveSixToOne");
+
+		GameObject tmp;
+
+		tmp = slot0;
+
+		slot0 = slot1;
+		slot1 = slot2;
+		slot2 = slot3;
+		slot4 = slot5;
+		slot5 = slot6;
+		slot6 = tmp;
 	}
 
 	public void RotateSongLeft()
@@ -68,7 +179,12 @@ public class DJManager : MonoBehaviour
 
 	void Update () 
 	{
+		if(cooldown > 0)
+			cooldown-=Time.deltaTime;
 
-	
+		if(Input.GetMouseButton (0))
+		{
+			//LeftSwipe();
+		}
 	}
 }
---
+++ DjApp/Assets/TouchManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour
+{
+	public DJManager dj;
+
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) 
+		{
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			
+			if(touchDeltaPosition.x < 0)
+			{
+				dj.SendMessage("LeftSwipe",touchDeltaPosition.x);
+			}
+		}
+
+	}
+}
