7bc92be785870dbe3aad537e88bc522d2e192606
Tyton <cleary@csumb.edu>
2015-03-15 08:56:30 -0700

    Separation of touches

--- DjApp/Assets/OtterDJ/Scripts/TouchManager.cs
+++ DjApp/Assets/OtterDJ/Scripts/TouchManager.cs
@@ -30,94 +30,98 @@ public class TouchManager : MonoBehaviour
 		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) 
 		{
 			Vector3 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			int touchy = Input.GetTouch (0).position.x;
+			if ( (frame.transform.position.y < 20 && touchy < 20) ) ||
+					  (frame.transform.position.y > -80 && touchy > -80) ){
 
-			if(touchDeltaPosition.x > 85)
-			{
-				touchDeltaPosition = new Vector3(85,touchDeltaPosition.y,touchDeltaPosition.z);
-			}
-			else
-			if(touchDeltaPosition.x < -85)
-			{
-				touchDeltaPosition = new Vector3(-85,touchDeltaPosition.y,touchDeltaPosition.z);
-			}
-			
-			//if(touchDeltaPosition.x < 0)
-			//{
-			//	dj.SendMessage("LeftSwipe",touchDeltaPosition.x);
-			//}
-			frame.Translate(new Vector3(touchDeltaPosition.x, 0, 0) );
-
-
-			if ( (frame.localPosition.x > -200) && (frame.localPosition.x < 200))
-			{
-				frame.eulerAngles = new Vector3(0, frame.localPosition.x / 4, 0);
+				if(touchDeltaPosition.x > 85)
+				{
+					touchDeltaPosition = new Vector3(85,touchDeltaPosition.y,touchDeltaPosition.z);
+				}
+				else
+				if(touchDeltaPosition.x < -85)
+				{
+					touchDeltaPosition = new Vector3(-85,touchDeltaPosition.y,touchDeltaPosition.z);
+				}
 				
-				//if(touchDeltaPosition != Vector3.zero)
-				//GetComponent<RectTransform>().Translate(new Vector3(touchDeltaPosition.x, 0, 0) );
-				
-				//frame.localScale = new Vector3(1,1,1)*((1 + 200 - (Mathf.Abs (frame.localPosition.x)) )* .008f) ;
-				//frame.localScale = new Vector3(1,1,1)*((1 + (200 - Mathf.Abs (frame.localPosition.x))/100)) ;
-				
-				
-			}
-			else
-			{
-				//float adj = (Mathf.Abs (frame.localPosition.x) - 200)/2.0f;
-				//GetComponent<RectTransform>().Translate(new Vector3(touchDeltaPosition.x, 0, 0) );
-
-			}
-
-			if(frame.localPosition.x < -451)
-			{
-
-				//if(!dj.farRight)
+				//if(touchDeltaPosition.x < 0)
 				//{
-				//	frame.localPosition = new Vector3(449,0,0);
-
+				//	dj.SendMessage("LeftSwipe",touchDeltaPosition.x);
 				//}
-				//else
-				frame.localPosition = new Vector3(dj.farRight.localPosition.x+100,frame.localPosition.y,0);
-				frame.eulerAngles = new Vector3(0, 50, 0);
+				frame.Translate(new Vector3(touchDeltaPosition.x, 0, 0) );
 
 
-				dj.farRight = frame;
-				dj.MoveMeRight(id);
-			}
-			else
-			if(frame.localPosition.x > 451)
-			{
-
-				//if(!dj.farRight)
-				//{
-				//	frame.localPosition = new Vector3(-449,0,0);
+				if ( (frame.localPosition.x > -200) && (frame.localPosition.x < 200))
+				{
+					frame.eulerAngles = new Vector3(0, frame.localPosition.x / 4, 0);
 					
-				//}
-				//else
-				frame.localPosition = new Vector3(dj.farLeft.localPosition.x-100,frame.localPosition.y,0);
-				frame.eulerAngles = new Vector3(0, -50, 0);
+					//if(touchDeltaPosition != Vector3.zero)
+					//GetComponent<RectTransform>().Translate(new Vector3(touchDeltaPosition.x, 0, 0) );
+					
+					//frame.localScale = new Vector3(1,1,1)*((1 + 200 - (Mathf.Abs (frame.localPosition.x)) )* .008f) ;
+					//frame.localScale = new Vector3(1,1,1)*((1 + (200 - Mathf.Abs (frame.localPosition.x))/100)) ;
+					
+					
+				}
+				else
+				{
+					//float adj = (Mathf.Abs (frame.localPosition.x) - 200)/2.0f;
+					//GetComponent<RectTransform>().Translate(new Vector3(touchDeltaPosition.x, 0, 0) );
 
-				dj.farLeft = frame;
-				dj.MoveMeLeft(id);
+				}
+
+				if(frame.localPosition.x < -451)
+				{
+
+					//if(!dj.farRight)
+					//{
+					//	frame.localPosition = new Vector3(449,0,0);
+
+					//}
+					//else
+					frame.localPosition = new Vector3(dj.farRight.localPosition.x+100,frame.localPosition.y,0);
+					frame.eulerAngles = new Vector3(0, 50, 0);
+
+
+					dj.farRight = frame;
+					dj.MoveMeRight(id);
+				}
+				else
+				if(frame.localPosition.x > 451)
+				{
+
+					//if(!dj.farRight)
+					//{
+					//	frame.localPosition = new Vector3(-449,0,0);
+						
+					//}
+					//else
+					frame.localPosition = new Vector3(dj.farLeft.localPosition.x-100,frame.localPosition.y,0);
+					frame.eulerAngles = new Vector3(0, -50, 0);
+
+					dj.farLeft = frame;
+					dj.MoveMeLeft(id);
+				}
+
+
+
+				/*
+				if(frame.localScale.magnitude < 1)
+				{
+					//frame.localScale = new Vector3(1,1,1);
+				}
+
+				if(frame.localScale.magnitude > 1.6f)
+				{
+					frame.localScale = new Vector3(1.6f,1.6f,1.6f);
+					
+				}
+				*/
+
 			}
 
 
-
-			/*
-			if(frame.localScale.magnitude < 1)
-			{
-				//frame.localScale = new Vector3(1,1,1);
-			}
-
-			if(frame.localScale.magnitude > 1.6f)
-			{
-				frame.localScale = new Vector3(1.6f,1.6f,1.6f);
-				
-			}
-			*/
-
+			frame.localPosition = new Vector3(frame.localPosition.x,frame.localPosition.y,0);
 		}
-
-
-		frame.localPosition = new Vector3(frame.localPosition.x,frame.localPosition.y,0);
 	}
 }
